58be5f628d74b28a43d30691c1410d94ecaa8f37
SXNGJUNHX <156043182+SXNGJUNHX@users.noreply.github.com>
2024-06-12 15:23:07 +0900

    update 24.06.12
    update 24.06.12

--- boot-security/src/main/java/com/test/bootsecurity/controller/AuthController.java
+++ boot-security/src/main/java/com/test/bootsecurity/controller/AuthController.java
@@ -1,5 +1,10 @@
 package com.test.bootsecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,6 +16,18 @@ public class AuthController {
 
         return "login";
     }
+    //로그 아웃
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/";
+    }
+
 
 }
 
